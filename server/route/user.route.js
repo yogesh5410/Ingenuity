715e755e32fa650cdd428e2b1665b8949d107097
Yogesh <yogesh5410official@gmail.com>
2025-06-07 16:55:42 +0530

    Update user.route.js

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { register, login, forgotPassword, getUserDetails, logout, updateProgress } from '../controllers/user.controller.js';
+import { register, login, forgotPassword, getUserDetails, logout, updateProgress, refreshToken } from '../controllers/user.controller.js';
 // import { admin } from '../middleware/admin.js';
 import auth from '../middleware/auth.js';
 
@@ -10,7 +10,8 @@ userRouter.post('/login', login);
 userRouter.post('/reset-password', forgotPassword);
 userRouter.get('/details', auth, getUserDetails);
 userRouter.get('/logout', auth, logout); 
-userRouter.put('/progress', auth, updateProgress)// Logout route
+userRouter.put('/progress', auth, updateProgress)
+userRouter.post('/refresh', refreshToken)
 // Route to get user details (protected route, requires authentication)
 
 export default userRouter
